689d5b80eae7553a5e97e5037fefca211b22005c
fabiodev <fabio@yubo.be>
2012-11-24 17:54:41 +0000

    Decommmission of Author pages Until further notice

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (!is_user_logged_in()) {
 <header class="page-header">
   <h1><a href="<?php echo get_permalink( $post->ID ); ?>" class="booty_post_title"><?php the_title(); ?></a><br>
 <small style="float:left"><?php the_time('F jS, Y') ?></small></h1>
-  <h4 style="float:right" ><i class="icon-pencil"></i> By <?php the_author_posts_link(); ?>.</h4><br>
+  <h4 style="float:right" ><i class="icon-pencil"></i> By <?php /* **disable author pages link** the_author_posts_link();*/ ?> <?php echo get_the_author(); ?> .</h4><br>
 </header>
 <div><?php the_content(__('(more...)')); ?></div>
 <footer>
