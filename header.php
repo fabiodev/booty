bc2956cab94994d42029f9455fd3e5ea92793a7f
Fabio Silva <fabio@yubo.be>
2013-09-10 19:48:00 +0100

    ADD: facebook JS SDK

--- header.php
+++ header.php
@@ -28,6 +28,16 @@
 	<?php include("lib/simple_html_dom.php"); ?>
 
 	<!-- <link rel="icon" href="http://84.23.214.33/wdev/favicon.ico" type="image/ico" /> -->
+//facebook
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/pt_PT/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+//facebook
 
     <?php //wp_head(); ?>
     </head>
